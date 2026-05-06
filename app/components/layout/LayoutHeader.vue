cbb50dbc5bafdd8b98f49220090d0c5ee9ac7ccb
Maulen <azikulovv@gmail.com>
2026-05-06 20:35:40 +0500

    feat: обновлен фон заголовка в компоненте LayoutHeader

--- app/components/layout/LayoutHeader.vue
+++ app/components/layout/LayoutHeader.vue
@@ -1,5 +1,5 @@
 <template>
-  <header class="p-6 text-center sticky top-0 bg-[#0F0F0F]/80 backdrop-blur-lg z-50">
+  <header class="p-6 text-center sticky top-0 bg-(--bg)/20 backdrop-blur-lg z-50">
     <h1 class="text-xl font-black uppercase tracking-widest italic">
       <slot />
     </h1>
