3bd9e5b49662eeaaba601ac5ac3d5f839254df2f
Maulen <azikulovv@gmail.com>
2026-05-16 00:21:44 +0500

    feat: refine viewport meta tag for improved mobile compatibility

--- app/app.vue
+++ app/app.vue
@@ -3,11 +3,7 @@ useHead({
   meta: [
     { name: 'apple-mobile-web-app-capable', content: 'yes' },
     { name: 'apple-mobile-web-app-status-bar-style', content: 'black-translucent' },
-    {
-      name: 'viewport',
-      content:
-        'width=device-width, initial-scale=1.0, maximum-scale=1.0, viewport-fit=cover, user-scalable=0',
-    },
+    { name: 'viewport', content: 'width=device-width, initial-scale=1, viewport-fit=cover' },
   ],
   link: [{ rel: 'apple-touch-icon', href: '/192.png' }],
 })
