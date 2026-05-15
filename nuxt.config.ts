0e2dad35bd939fa53106bbfd787240297479f1ff
Maulen <azikulovv@gmail.com>
2026-05-16 00:27:27 +0500

    feat: update start_url in PWA manifest for improved source tracking

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -42,7 +42,7 @@ export default defineNuxtConfig({
       background_color: '#0a0c12',
       display: 'standalone',
       orientation: 'portrait',
-      start_url: '/',
+      start_url: '/?source=pwa',
       scope: '/',
 
       icons: [
