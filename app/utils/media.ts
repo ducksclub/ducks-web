85ee89cff368b4fcfe0ccd4edfa4a341680416ca
Maulen <azikulovv@gmail.com>
2026-06-30 17:05:46 +0500

    feat(media): simplify renderPicture function by removing URL checks

--- app/utils/media.ts
+++ app/utils/media.ts
@@ -1,9 +1,4 @@
 export const renderPicture = (url?: string | null) => {
-  if (!url) return ''
-  if (/^https?:\/\//i.test(url)) return url
-
   const config = useRuntimeConfig()
-  const mediaBase = config.public.mediaBase || config.public.apiBase.replace(/\/api\/?$/, '')
-
-  return mediaBase.replace(/\/$/, '') + url
+  return config.public.mediaBase + url
 }
